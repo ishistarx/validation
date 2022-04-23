8a3aa8951b8b28dcf818125245aa3c618e443de7
ishistarx <ishitabiswas2002@gmail.com>
2022-04-23 19:48:23 +0530

    Validation in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'validation';
+  form = new FormGroup({
+    uname : new FormControl('',[Validators.required, Validators.minLength(3)]),
+    email : new FormControl('',[Validators.required, Validators.email]),
+    body : new FormControl('',[Validators.required]),
+    phone : new FormControl('',[Validators.required, Validators.minLength(10)]),
+    date : new FormControl('',[Validators.required])
+  });
+  get f(){
+    return this.form.controls;
+  }
+  submit(){
+    console.log(this.form.value);
+  }
+
 }
